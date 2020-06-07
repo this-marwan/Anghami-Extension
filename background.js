306a2e0f23f787e0c987a37a6466e188269ec35a
this-marwan <mrg08@mail.aub.edu>
2020-06-07 13:12:58 +0300

    updated the app visually, error handling, improved code reliability

--- background.js
+++ background.js
@@ -2,61 +2,14 @@ const regex = RegExp(".*://.*\.youtube\.com/.*"); //regex to checjk if we vist y
 youtubeKey = ""; //this our Youtube API key
 anghamiKey = "";
 var finalLink = "";
-//Our event listeners 1. button click 2.we visit youtube
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-
-    let url = tabs[0].url;
-    if (regex.test(url)) { //check if we visted youtube
-      console.log("Button triggered : " + url);
-      videoID = getParameterByName('v',url);
-      console.log("Youtube! ID " + videoID);
-      if (videoID != null){
-      //get video title
-      getYoutubeTitle(videoID);
-
-    }
-    else {
-      console.log("Not valid url !");
-    }
-    }
-    else
-    {
-      console.log("Not valid url !");
-    }
-
-    });
-});
-
-//called whenever the URL changes (new tab, redirect, ...)
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  console.log(changeInfo.url);
-    if (regex.test(changeInfo.url)) { //check if the new URL is on youtube or not
-      //get the video ID from the url:
-      videoID = getParameterByName('v',changeInfo.url);
-      console.log("Youtube! ID " + videoID);
-      if (videoID != null){
-      //get video title
-      getYoutubeTitle(videoID);
-    }
-      else {
-        console.log("Not valid url!");
-      }
-    }
-    else
-    {
-      console.log("Not valid url!");
-    }
-});
+var status;
 
 //searchAnghami
-function searchAnghami(title)
- {
-   console.log("Searching Anghami for :" + title);
+const searchAnghami = function(title){
+  console.log("getting title " + title);
+  return new Promise(function(resolve, reject) {
    request ='https://bus.anghami.com/public/search?query=' + title + "&searchtype=song";
-
+   shareURL = ""
    var xhr = new XMLHttpRequest();
    xhr.open('GET', request, true);
    xhr.setRequestHeader("XAT", 'interns');
@@ -66,22 +19,23 @@ function searchAnghami(title)
        
    function processRequest(e) {
      if (xhr.readyState == 4 && xhr.status == 200) {
-         console.log(xhr.responseText);
            var response = JSON.parse(xhr.responseText);
-           // console.log(response["results"][0]["title"]);
-           // console.log(response["results"][0]["id"]);
-
-          // shareURL = "https://play.anghami.com/song/" + response["results"][0]["id"];
+           console.log(response);
+           if (!response.hasOwnProperty("results")) {
+             status = "error"
+             reject({URL:shareURL, STATUS : status})
+           }
            shareURL = "https://play.anghami.com/song/" + response["results"][0]["id"];
-           window.open("popup.html", "extension_popup", "width=300,height=50,status=no,scrollbars=yes,resizable=no");
-           finalLink = shareURL;
-          console.log(shareURL);
+           status = "success";
+           resolve({URL:shareURL, STATUS : status});
          }
-   }
- }
+       }
+     })
+}
 
-function getYoutubeTitle(videoID)
-{
+const getYoutubeTitle = function(videoID) {
+  console.log("getting song " + videoID);
+  return new Promise(function(resolve, reject) {
   //get information about video, specifically we want to know if it is a music video and the video title as well
   request = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id="+ videoID + "&key=" + youtubeKey;
   //generic http request
@@ -93,36 +47,61 @@ function getYoutubeTitle(videoID)
   function processRequest(e) {
     if (xhr.readyState == 4 && xhr.status == 200) { //good request
           var response = JSON.parse(xhr.responseText);
-          //console.log(response); //for debugging
-
+          console.log(response);
+          // if (!response["items"][0]["snippet"]["categoryId"])
+          // {
+          //   status = "error"
+          //   reject({URL:finalLink, STATUS : status})
+          // }
           categoryId = response["items"][0]["snippet"]["categoryId"];
           title = response["items"][0]["snippet"]["title"];
           //categoryId 10 is youtube's category for music videos
           if (categoryId == 10) //if the video is a music video
           {
-            console.log("TITLE : " + title);
-            //we have to devote the anghami search request to the background script to comply with chrome's security policy
-            //check it out: https://www.chromium.org/Home/chromium-security/extension-content-script-fetches
-
-            //search for it on anghami
-            console.log("Going to search for : " + title);
-            searchAnghami(title);
+            resolve(titleCleaner(title))
           }
           else {
-            console.log("Not a song");
+            console.log("Not song");
+            status = "notsong"
+            reject({URL:finalLink, STATUS : status})
           }
         }
-
-  }
-
+      }
+})
 }
+
 //eventlistener to popup.js
+
 chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
 {
-    if( request.greeting === "GetURL" )
-    {
-            sendResponse( {URL:finalLink} );
+  var videoID;
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    if (regex.test(url)) { //check if we visted youtube
+      videoID = getParameterByName('v',url);
     }
+    else {
+      status = "youtube";
+      sendResponse( {URL:finalLink, STATUS : status})
+      return;
+    }
+
+      if (videoID != null){
+      //get video title
+        getYoutubeTitle(videoID)
+                  .then(searchAnghami)
+                  .then(sendResponse)
+                  .catch(sendResponse)
+      }
+      else {
+      console.log("Not valid url !");
+      status = "notsong"
+      sendResponse( {URL:finalLink, STATUS : status})
+      }
+
+  })
+
+return true;
 });
 
 
@@ -136,4 +115,19 @@ chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
      if (!results) return null;
      if (!results[2]) return '';
      return decodeURIComponent(results[2].replace(/\+/g, ' '));
+ }
+
+ function titleCleaner(title){
+   //this removes uneccessary common elements of a youtube video title - supposedly will improve search results
+   title = title.replace(/[[(]?(Official)? (Music )?(Video|Audio)[\])]?/g,"")
+   title = title.replace(/\\(Official\\)|\\(Audio\\)/g,"")
+   title = title.replace(/\(?Official\)?/g,"")
+   title = title.replace(/-/g," ")
+   title = title.replace(/\(/g," ")
+   title = title.replace(/\)/g," ")
+   title = title.replace(/\s+/g," ")
+   console.log("title" + title);
+
+
+   return title;
  }
--- popup.js
+++ popup.js
@@ -1,11 +1,23 @@
 function getURL() {
-    chrome.runtime.sendMessage({greeting: "GetURL"},
-        function (response) {
+    chrome.runtime.sendMessage({greeting: "GetURL"}, function(response){respond(response)})
+  function respond (response) {
+    console.log(response);
+          if (response.STATUS == "success"){
             tabURL = response.URL;
-            console.log(tabURL);
-            console.log("We're here");
-            document.getElementById("link").innerHTML = document.getElementById("link").innerHTML + "<a href=" + tabURL + ">" + tabURL + "</a>";
-        });
+            document.getElementById("link").innerHTML = "Continue jamming on the go right here : <a href=" + tabURL + ">" + tabURL + "</a>";
+          }
+          else if (response.STATUS == "youtube"){
+            document.getElementById("link").innerHTML = "Not a valid youtube link";
+          }
+          else if (response.STATUS == "notsong"){
+            document.getElementById("link").innerHTML = "Doesn't seem to be a song";
+          }
+          else if (response.STATUS == "error"){
+            document.getElementById("link").innerHTML = "Woops something went wrong :/";
+          }
+          else{
+            document.getElementById("link").innerHTML = "Just a sec";
+          }
+        };
 }
-
 getURL();
